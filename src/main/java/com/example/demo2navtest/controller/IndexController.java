63c4611bc55f7681b5b224110c2ba6e20df0c4cf
Kyaw Kyaw Lwin <kyawkyawlwinthant@gmail.com>
2021-09-06 14:20:09 +0630

    ajax submit

--- src/main/java/com/example/demo2navtest/controller/IndexController.java
+++ src/main/java/com/example/demo2navtest/controller/IndexController.java
@@ -4,8 +4,9 @@ package com.example.demo2navtest.controller;
 import com.example.demo2navtest.ds.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class IndexController {
@@ -16,10 +17,11 @@ public class IndexController {
         model.addAttribute("student",new Student());
         return "page1";
     }
-    @PostMapping("/save-student")
-    public String saveStudent(Student student){
-
+    @RequestMapping("/save-student")
+    public String saveStudent(Student student, @RequestBody String  id){
+        String[] testid=id.split("=");
         System.out.println(student);
+        System.out.println(testid[1]);
         return "page2";
     }
 
